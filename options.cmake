18de5cb5b9819f478cba56f53b344ce645defcb2
Tarnith <me@tarnith.com>
2024-05-30 10:54:46 -0600

    Ensure pugl_static is on, move opengl on in options

--- options.cmake
+++ options.cmake
@@ -3,13 +3,12 @@
 
 # Implemented 
 option(PUGL_STATIC "Static build" ON)
-
+option(BUILD_OPENGL "Enable support for the OpenGL graphics API" ON)
 
 # Not implemented
 option(BUILD_EXAMPLES "Build example programs" OFF)
 option(BUILD_DOCS "Build documentation" OFF)
 option(BUILD_DOCS_CPP "Attempt to build C++ documentation for development" OFF)
-option(BUILD_OPENGL "Enable support for the OpenGL graphics API" ON)
 option(BUILD_STUB "Build stub backend" OFF)
 option(SUPPORT_XCURSOR "Support changing the cursor on X11" OFF)
 option(SUPPORT_XRANDR "Support accessing the refresh rate on X11" OFF)
